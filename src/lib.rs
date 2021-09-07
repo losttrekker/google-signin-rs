d8bb43c12d7c104fa2a666fdbac19d9075f86591
Jonathan Kellenberg <jkellenberg@singularity6.com>
2021-09-07 15:40:15 -0700

    removed legacy tokio 0.2 runtime.

--- src/lib.rs
+++ src/lib.rs
@@ -40,22 +40,14 @@
 //! # }
 //! ```
 
-extern crate hyper;
-#[cfg(feature = "with-rustls")]
-extern crate hyper_rustls;
-#[cfg(feature = "with-openssl")]
-extern crate hyper_openssl;
 extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_json;
 
 mod client;
 mod error;
 mod token;
 
-pub use client::Client;
 pub use client::CachedCerts;
+pub use client::Client;
 pub use error::Error;
 pub use token::IdInfo;
 
